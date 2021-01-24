7ce6cd429c6660e8cc0bb63f4c617aa9540dee07
johanWijanarko <webjohan123@gmail.com>
2021-01-24 17:58:38 +0700

    req kebalik

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ const response = require('./res'); // panggil file rsponse
 const connection = require('./koneksi'); // panngil file koneksi
 
 exports.index = function(req, res){
-    response.ok("apliksi berjalan ", res)
+    response.ok("apliksi berjalan ",res)
 }
