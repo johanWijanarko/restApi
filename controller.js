9cc16b81da8d3f70eb1558912cb1fb0453e68eb0
johanWijanarko <webjohan123@gmail.com>
2021-01-24 17:56:48 +0700

    cek

--- controller.js
+++ controller.js
@@ -3,6 +3,6 @@
 const response = require('./res'); // panggil file rsponse
 const connection = require('./koneksi'); // panngil file koneksi
 
-exports.index = function(res, req){
+exports.index = function(req, res){
     response.ok("apliksi berjalan ", res)
 }
