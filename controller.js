0783956efa8a46ff286c0a21d0f51bc7d33f3e65
johanWijanarko <webjohan123@gmail.com>
2021-01-24 21:24:10 +0700

    get data dan query controler

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ const connection = require('./koneksi'); // panngil file koneksi
 exports.index = function(req, res){
     response.ok("apliksi berjalan ",res)
 }
+
+// getAll data 
+exports.getAlldata = function(req, res){
+    connection.query('SELECT * from tb_kriteria where del_st = 1', function(error, rows, fields){
+        if(error){
+            connection.log(err)
+        }else {
+            response.ok(rows, res)
+        }
+    })
+}
