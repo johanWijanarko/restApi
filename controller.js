3f4bdc96fa6fa27d5d3d44cf24823b15d3961bd1
johanWijanarko <webjohan123@gmail.com>
2021-01-24 22:41:41 +0700

    penmabahan cryptoid agar id atomatis

--- controller.js
+++ controller.js
@@ -1,4 +1,6 @@
 'use strict';
+// const uuidV1 = require('uuid');
+const crypto = require("crypto");
 
 const response = require('./res'); // panggil file rsponse
 const connection = require('./koneksi'); // panngil file koneksi
@@ -23,6 +25,23 @@ exports.getAlldata = function(req, res){
 exports.getDataByid = function(req, res){
     let id = req.params.id;
     connection.query('SELECT * from tb_kriteria where del_st = 1 and id_kriteria = ?',[id], function(error, rows, fields){
+        if(error){
+            console.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
+}
+
+exports.sendData = function(req, res){
+    let nama_kriteria = req.body.nama_kriteria;
+    let bobot = req.body.bobot;
+    let id = crypto.randomBytes(16).toString("hex");
+    const del_st = 1;
+    // console.log(id);
+
+    connection.query('insert into tb_kriteria (id_kriteria, kriteria, bobot, del_st) values(?,?,?,?)', [id, nama_kriteria, bobot, del_st],
+       function(error, rows, fields){
         if(error){
             console.log(error)
         } else {
